242a301648851dd8cd41bd5f9de8b52291993477
Noora Aulia H <nooraaulia20h@gmail.com>
2026-08-11 20:44:10 +0700

    refactor(frontend): move app routes from dynamic [locale] to id folder

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,17 +1,12 @@
-import createNextIntlPlugin from "next-intl/plugin";
 import type { NextConfig } from "next";
-
-const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
 
-
-  // Aktifkan apabila project menggunakan next/image.
   images: {
     unoptimized: true,
   },
 };
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
